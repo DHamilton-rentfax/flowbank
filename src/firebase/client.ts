a5f3620b9b539dd299ffd7def64466db84dfb3ea
Dominique Hamilton <UrbnDesignz@gmail.com>
2025-08-05 05:03:39 +0000

    File changes

--- src/firebase/client.ts
+++ src/firebase/client.ts
@@ -5,9 +5,9 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig: FirebaseOptions = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "flowbank.firebaseapp.com",
-  projectId: "flowbank",
-  storageBucket: "flowbank.appspot.com",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
