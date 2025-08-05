8d10e9808498ca6bdc5274e22660b6ff1b057635
Dominique Hamilton <UrbnDesignz@gmail.com>
2025-08-05 00:42:08 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/firebase/client.ts
+++ src/firebase/client.ts
@@ -1,15 +1,14 @@
-
 import { initializeApp, getApps, getApp, type FirebaseOptions } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig: FirebaseOptions = {
-  apiKey: "AIzaSyC9BfxPjqxsL96Op0MhV2IidqNynSvdjjg",
-  authDomain: "autoallocator.firebaseapp.com",
-  projectId: "autoallocator",
-  storageBucket: "autoallocator.appspot.com",
-  messagingSenderId: "280963293966",
-  appId: "1:280963293966:web:878190c5bfae3d00e7ad5f",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
