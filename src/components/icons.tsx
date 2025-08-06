c6b9c6b5a13cbbf5bcf3acf1c3955447ef3c9e77
Dominique Hamilton <UrbnDesignz@gmail.com>
2025-08-06 05:56:36 +0000

    here is my logo can you replace the image by the word "Flow Bank" on the

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -4,29 +4,15 @@ import type { SVGProps } from "react";
 export function Logo(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
-      xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
       viewBox="0 0 24 24"
       fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <path d="M12 8v5" />
-      <path d="M10.5 10.5c.33-1.5.33-3 0-4.5" />
-      <path d="M13.5 10.5c-.33-1.5-.33-3 0-4.5" />
-      <path d="M7 16c-1-1.5-1-3.5 0-5" />
-      <path d="M17 16c1-1.5 1-3.5 0-5" />
-      <path d="M4 22c-1.5-1.5-2-4-2-6" />
-      <path d="M22 16c0-2-1-5-2-6" />
-      <path d="M12 2v2" />
-      <path d="M6 14c-1-1-1.5-2.5-1.5-4" />
-      <path d="M18 14c1-1 1.5-2.5 1.5-4" />
-      <path d="M22 2l-5 5" />
-      <path d="M2 2l5 5" />
+        <path d="M17.477 3.5H6.523c-1.67 0-3.023 1.353-3.023 3.023V9.67h10.954V6.523c0-1.67-1.353-3.023-3.023-3.023Z" fill="hsl(var(--secondary))"/>
+        <path d="M14.454 12.72H3.5v4.757c0 1.67 1.353 3.023 3.023 3.023h4.757V12.72h3.174Z" fill="hsl(var(--primary))"/>
     </svg>
   );
 }
